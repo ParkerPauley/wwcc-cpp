f8c1e0702c1c8d5107a0e9ddc148768d12a67b20
ParkerPauley <parkerpauley10@gmail.com>
2025-04-12 19:32:41 -0700

    Create makefiledemo.cpp

---
+++ makefiledemo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    
+    std::cout << "This Program was complied using a Makefile!" << std::endl;
+
+    return 0;
+}
