5db6b0b924619f5c31250238ac4cf70433ee70ef
ParkerPauley <parkerpauley10@gmail.com>
2025-06-13 15:28:52 -0700

    Update Product.h

--- week7/Encapsulation/Product.h
+++ week7/Encapsulation/Product.h
@@ -1,1 +1,31 @@
 
+#ifndef PRODUCT_H
+#define PRODUCT_H
+#include <string>
+#include <vector>
+
+class Product {
+private:
+    int id;
+    std::string name;
+    double price;
+    int quantity;
+
+public:
+    Product(int id, const std::string& name, double price, int quantity);
+
+    int getId() const;
+    std::string getName() const;
+    double getPrice() const;
+    int getQuantity() const;
+
+    void setPrice(double price);
+    void setQuantity(int quantity);
+
+    void addStock(int amount);
+    void removeStock(int amount);
+
+    void display() const;
+};
+
+#endif
