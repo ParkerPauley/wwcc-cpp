e589d658852b2ddf644c491a016cf4458a425664
ParkerPauley <parkerpauley10@gmail.com>
2025-06-13 15:28:58 -0700

    Update Inventory.h

--- week7/Encapsulation/Inventory.h
+++ week7/Encapsulation/Inventory.h
@@ -1,1 +1,22 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include "Product.h"
+
+class Inventory {
+private:
+    std::vector<Product> products;
+
+public:
+    void addProduct(const Product& p);
+    void removeProduct(int id);
+    Product* findProductById(int id);
+    Product* findProductByName(const std::string& name);
+    void listAllProducts() const;
+    void listOutOfStock() const;
+    void listLowStock(int threshold) const;
+    void updateProduct(int id);
+    double totalInventoryValue() const;
+};
+
+#endif
 
